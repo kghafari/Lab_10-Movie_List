a1728c59a112a0fd388b5929876335538a191ebc
kyle <kyleghafari@gmail.com>
2020-01-30 14:51:15 -0500

    First commit. Just setting up.

---
+++ Lab_10-Movie_List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
