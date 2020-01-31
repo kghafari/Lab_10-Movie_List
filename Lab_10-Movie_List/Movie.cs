66a9ee5a5b15569f78d9af4756d81230c923a3c4
kyle <kyleghafari@gmail.com>
2020-01-30 20:27:22 -0500

    Core functionality added

---
+++ Lab_10-Movie_List/Movie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_Movie_List
+{
+    class Movie
+    {
+        //fields
+        private string title;
+        private string category;
+
+        //properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        //constructors
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        //methods
+        public static void PrintMoviesByType(List<Movie> movieList, string categorySelected)
+        {
+            categorySelected.ToLower();
+            foreach (Movie movie in movieList)
+            {
+                if (categorySelected == movie.category.ToLower() || categorySelected == movie.category)
+                {
+                    Console.WriteLine($"{movie.title}");
+                }
+            }
+        }
+    }
+}
--- Lab_10-Movie_List/Program.cs
+++ Lab_10-Movie_List/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab_10_Movie_List
 {
@@ -6,7 +7,63 @@ namespace Lab_10_Movie_List
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            //task: list movies by category
+
+            //store a list of 10 Movies (object data type Movie)
+
+            List<Movie> movieList = new List<Movie>
+            {
+                new Movie("The Shining", "Horror"),
+                new Movie("Gangs of New York", "Drama"),
+                new Movie("The Matrix", "Scifi"),
+                new Movie("Gattica", "Scifi"),
+                new Movie("Isle of Dogs", "Animated"),
+                new Movie("A Dark Song", "Horror"),
+                new Movie("Glee: The Movie", "Horror"),
+                new Movie("Howl's Moving Castle", "Animated"),
+                new Movie("The Irishman", "Drama"),
+                new Movie("Rosemary's Baby", "Horror"),
+            };
+
+
+            bool userContinue = true;
+            while (userContinue)
+            {
+              string userInput = GetUserInput("What genere are you interested in?\nHorror\tScifi\tDrama\tAnimated");
+
+                Console.WriteLine();
+                Movie.PrintMoviesByType(movieList, userInput);
+                Console.WriteLine();
+
+                userContinue = KeepGoing("Would you like to continue? (y/n)", "n", "y");
+            }
+
         }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static bool KeepGoing(string message, string option1, string option2)
+        {
+            string keepGoing = GetUserInput(message);
+            if (keepGoing == option1)
+            {
+                return false;
+            }
+            else if (keepGoing == option2)
+            {
+                return true;
+            }
+            else
+            {
+
+                return KeepGoing(message, option1, option2);
+            }
+        }
+
     }
 }
